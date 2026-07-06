13b1da686749ad2205dd3709ee0a4cec293369f2
uribebusiness-bit <uribebusiness@gmail.com>
2026-07-05 21:06:40 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'etdt-v19';
+const CACHE_NAME = 'etdt-v20';
 const ASSETS = [
   './', './app.html', './offline.html', './manifest.json',
   './icons/icon-192.png', './icons/icon-192-maskable.png',
